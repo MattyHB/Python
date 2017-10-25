bca72ea70e3421e8dc06297b530fde47c86f670b
Matthew Beals <mattmanbeals@gmail.com>
2017-10-25 13:44:07 -0400

    b

--- Prog4/json2excel.py
+++ Prog4/json2excel.py
@@ -1,11 +1,14 @@
+# −−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−
+# File: json2excel.py
+# Author: Matthew Beals User ID: Mbeal872 Class: CPS 110
+# Desc: This program takes a .json file or json URL and converts it
+# into a working Excel file (.xlsx) format.
+# −−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−
+
 import sys
 import os
-import pandas as pd
-import openpyxl
-import xlsxwriter
+import pandas
 import requests
-
-
 
 if len(sys.argv) != 3:
     print('Usage: python json2excel.py filename-or-url spreadsheet-filename')
@@ -19,7 +22,4 @@ if jsonpoint[:3] == "http":
     jsonpoint = requests.get(sys.argv[1])
     jsonpoint = jsonpoint.json
 
-pd.read_json(jsonpoint).to_excel(xceldoc)
-
-
-#https://www.dallasopendata.com/resource/are8-xahz.json
+pandas.read_json(jsonpoint).to_excel(xceldoc)
