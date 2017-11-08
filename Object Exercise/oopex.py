60a5ff442a79655087177200f830ef8518cc8b75
mbeal872 <mbeal872@students.bju.edu>
2017-11-07 22:27:46 -0500

    eggs

--- Object Exercise/oopex.py
+++ Object Exercise/oopex.py
@@ -27,30 +27,31 @@ class Address:
                 return False     
         return True
 
-
     def __repr__(self):
         return 'Name: {0} , Email: {1}'.format(self.__name, self.__email)
 
 
 class AddressBook:
 
-    def __init__(self, addresses: dict):
-        self.__addresses = {addresses}
+    def __init__(self):
+        self.__addresses = {}
 
-    def insert(self, Address):
-        self.__addresses = self.__addresses + (Address)
+    def insert(self, a):
+        self.__addresses[a.getName()] = a
 
     def getCount(self):
-        num = 0
+        return len(self.__addresses)
+    
+    def get(self, name: str):
+        if name in self.__addresses:
+            return self.__addresses[name]
+        else:
+            return None
+    def getNames(self):
+        AlphList = []
         for i in self.__addresses:
-            num += 1
-        return num
-
-
-
-
-
-
+            AlphList.append(i)
+        return AlphList
 
 # ------ Test ------
 
@@ -70,5 +71,30 @@ def test_Address():
     assert addr.getName() == "Lydia"
     assert addr.getEmail() == 'lydiagmail!@.com'
     assert addr.isValidEmail() == False
-    
-# Finish the header
+
+def test_AddressBook():
+    book = AddressBook()
+    assert book.getCount() == 0
+
+    book.insert(Address('Frank', 'frank@bju.edu'))
+    assert book.getCount() == 1
+
+    book.insert(Address('Helen', 'helen@bju.edu'))
+    assert book.getCount() == 2
+
+    book.get('Frank') == Address('Frank', 'frank@bju.edu')
+    book.get('GiveMeAnA+OnThisAssignment') == None
+
+    names = book.getNames()
+    assert names == ['Frank', 'Helen']
+
+    addr = book.get('Frank')
+    assert addr != None
+    assert addr.getName() == 'Frank'
+    assert addr.getEmail() == 'frank@bju.edu'
+
+    addr = book.get('Helen')
+    assert addr.getEmail() == 'helen@bju.edu'
+
+    addr = book.get('Joey')
+    assert addr == None
