58bb24c471480e39ae8fd45659ca04dcec788b9c
Matthew Beals <mattmanbeals@gmail.com>
2017-12-02 05:26:58 -0500

    gg

--- Prog6/isnewerthan.py
+++ Prog6/isnewerthan.py
@@ -1,2 +1,15 @@
 import sys
 import os
+
+try:
+    checkDir = sys.argv[1]
+except Exception as e:
+    print('Usage: isnewerthan.py C:/destination ')
+    exit(1)
+
+for checkFile in os.listdir(checkDir):
+    path = (checkDir + '\\' + checkFile)
+    for line in sys.stdin:
+        line = line.rstrip('\n')
+        if os.path.getmtime(line) > os.path.getmtime(path):
+            print(line)
